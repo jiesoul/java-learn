4e826343411142373268c7ae35059e809c6c6ed4
jiesoul <jiesoul@gmail.com>
2018-06-29 03:52:45 +0800

    problem

---
+++ src/main/java/algo4/TopM.java
@@ -0,0 +1,26 @@
+package algo4;
+
+import edu.princeton.cs.algs4.*;
+
+public class TopM {
+
+    public static void main(String[] args) {
+        int M = 5;
+        
+        MinPQ<Transaction> pq = new MinPQ<>(M+1);
+        while (StdIn.hasNextLine()) {
+            pq.insert(new Transaction(StdIn.readLine()));
+            if (pq.size() > M) {
+                pq.delMin();
+            }
+        }
+
+        Stack<Transaction> stack = new Stack<>();
+        while (!pq.isEmpty()) {
+            stack.push(pq.delMin());
+        }
+        for (Transaction t : stack) {
+            StdOut.println(t);
+        }
+    }
+}
--- src/main/java/leetcode/RegularExpressionMatching.java
+++ src/main/java/leetcode/RegularExpressionMatching.java
@@ -2,33 +2,53 @@ package leetcode;
 
 public class RegularExpressionMatching {
     public boolean isMatch(String s, String p) {
-
-        if (!p.contains(".") && !p.contains("*")) {
-            return s.equals(p);
-        } else if (p.contains(".*")) {
-            return true;
-        } else {
-            boolean match = false;
-
-            for (int i=0; i < s.length(); i++) {
-                if (p.length() <= 0) {
+        int sl = s.length();
+        int pl = p.length();
+        if (sl == 0) {
+            if (pl == 0) {
+                return true;
+            } else {
+                if (pl > 1 && p.charAt(1) == '*') {
+                    return isMatch(s, p.substring(2));
+                } else {
                     return false;
                 }
-                char c = s.charAt(i);
-                int idx = p.indexOf(c);
-                if (idx == -1) {
-                    idx = p.indexOf(".");
-                    if (idx == -1) {
+
+            }
+        } else {
+            if (pl == 0) {
+                return false;
+            } else {
+                char sc = s.charAt(0);
+                char pc = p.charAt(0);
+                if (sc == pc || pc == '.') {
+                    if (pl > 1 && p.charAt(1) == '*') {
+                        return isMatch(s.substring(1), p);
+                    } else {
+                        return isMatch(s.substring(1), p.substring(1));
+                    }
+                } else {
+                    if (pl > 1 && p.charAt(1) == '*') {
+                        return isMatch(s, p.substring(2));
+                    } else {
                         return false;
                     }
                 }
-                for (int j=0; j < p.length(); j++) {
-                    char a = p.charAt(j);
-
-                }
             }
-            return match;
         }
 
+
+    }
+
+    public static void main(String[] args) {
+        RegularExpressionMatching matching = new RegularExpressionMatching();
+        System.out.println(matching.isMatch("aaa", "a*a"));
+        System.out.println(matching.isMatch("aab", "c*a*b"));
+        System.out.println(matching.isMatch("ab", ".*c"));
+        System.out.println(matching.isMatch("aaa", "aaaa"));
+        System.out.println(matching.isMatch("mississippi", "mis*is*p*."));
+        System.out.println(matching.isMatch("aa", "a*"));
+        System.out.println(matching.isMatch("aa", "a"));
+        System.out.println(matching.isMatch("ab", ".*"));
     }
 }
