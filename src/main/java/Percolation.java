0080a67a8a5c74d6ee1f4dae397eac4024a5e73a
jiesoul <jiesoul@gmail.com>
2018-07-02 17:10:43 +0800

    gogo

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,15 +1,25 @@
 public class Percolation {
-    public Percolation (int n) {
 
+    boolean[][] sites;
+
+    public Percolation (int n) {
+        sites = new boolean[n][n];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sites[i][j] = false;
+            }
+        }
     }
 
     public void open(int row, int col) {
-
+        if (!sites[row][col]) {
+            sites[row][col] = true;
+        }
     }
 
     public boolean isOpen(int row, int col) {
 
-        return false;
+        return sites[row][col];
     }
 
     public boolean isFull(int row, int col) {
