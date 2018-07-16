192f2286d2ec5f5f6b9da671136b88e8fa8f2e26
jiesoul <jiesoul@gmail.com>
2018-07-16 22:36:02 +0800

    bst

--- src/main/java/leetcode/ReverseNodesinkGroup.java
+++ src/main/java/leetcode/ReverseNodesinkGroup.java
@@ -3,11 +3,34 @@ package leetcode;
 public class ReverseNodesinkGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode cur = head;
-        for (int i = 0; i < k; i++) {
-
+        if (head == null) return null;
+        ListNode[] nodes = new ListNode[k];
+        int cnt = 0;
+        while (cnt < k && head != null) {
+            ListNode temp = head;
+            head = head.next;
+            temp.next = null;
+            nodes[cnt] = temp;
+            cnt++;
         }
-        return head;
+        ListNode result = null;
+        if (cnt == k) {
+            result = nodes[0];
+            result.next = reverseKGroup(head, k);
+            for (int i = 1; i < k; i++) {
+                nodes[i].next = result;
+                result = nodes[i];
+            }
+        } else {
+            result = nodes[cnt-1];
+            for (int i = cnt - 2; i >= 0; i--) {
+                nodes[i].next = result;
+                result = nodes[i];
+            }
+        }
+
+
+        return result;
     }
 
     public ListNode reverse(ListNode head) {
@@ -15,9 +38,27 @@ public class ReverseNodesinkGroup {
         ListNode last = null;
         while (head != null) {
             ListNode temp = head;
-            temp.next = null;
             head = head.next;
+            temp.next = last;
+            last = temp;
+
         }
         return last;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        ListNode listNode = new ReverseNodesinkGroup().reverseKGroup(n1, 2);
+        System.out.println(listNode);
     }
 }
---
+++ src/test/java/leetcode/RemoveDuplicatesfromSortedArrayTest.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemoveDuplicatesfromSortedArrayTest {
+
+    RemoveDuplicatesfromSortedArray a = new RemoveDuplicatesfromSortedArray();
+
+    @Test
+    public void removeDuplicatesTest() {
+        int[] n1 = {1,1,2};
+        assertEquals(2, a.removeDuplicates(n1));
+
+        int[] n2 = {0,0,1,1,1,2,2,3,3,4};
+        assertEquals(5, a.removeDuplicates(n2));
+    }
+}
