b83b6a99d3404f4ad712150d173436392d8d0b1a
jiesoul <jiesoul@gmail.com>
2018-07-23 17:52:23 +0800

    java 36

---
+++ src/main/java/java36/StampedSample.java
@@ -0,0 +1,19 @@
+package java36;
+
+import java.util.concurrent.locks.StampedLock;
+
+public class StampedSample {
+
+    private final StampedLock s1 = new StampedLock();
+
+    void mutate() {
+        long stamp = s1.writeLock();
+        try {
+//            write();
+        } finally {
+            s1.unlockWrite(stamp);
+        }
+    }
+
+
+}
