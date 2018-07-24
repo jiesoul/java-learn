b33527b8bb8b2a59af57571d0a843ee8c3ec4fdb
jiesoul <jiesoul@gmail.com>
2018-07-24 17:59:54 +0800

    java core

---
+++ src/main/java/corejava/ClassTest.java
@@ -0,0 +1,8 @@
+package corejava;
+
+public class ClassTest {
+
+    public static void main(String[] args) {
+        System.out.println(int.class);
+    }
+}
---
+++ src/main/java/corejava/objectAnalyzer/ObjectAnalyzerTest.java
@@ -0,0 +1,13 @@
+package corejava.objectAnalyzer;
+
+import java.util.ArrayList;
+
+public class ObjectAnalyzerTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> squares = new ArrayList<>();
+        for (int i = 0; i <= 5; i++) {
+            squares.add(i * i);
+        }
+        System.out.println(new ObjectAnalyzer().toString(squares));
+    }
+}
