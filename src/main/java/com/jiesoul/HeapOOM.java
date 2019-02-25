596b2f5a94a643619b3ecc19f96c3dd6684fefcb
zhangyunjie <jiesoul@gmail.com>
2019-02-25 13:34:06 +0800

    gradle upgrade

--- src/main/java/com/jiesoul/HeapOOM.java
+++ src/main/java/com/jiesoul/HeapOOM.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class HeapOOM {
 
     static class OOMObject {
-
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/jiesoul/leetcode/SubstringAndConcatenationOfAllWords.java
@@ -0,0 +1,21 @@
+package com.jiesoul.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA. Description:
+ *
+ * @Author: JIESOUL
+ * @Date: 2019-02-25
+ * @Time: 10:34
+ */
+public class SubstringAndConcatenationOfAllWords {
+
+  public List<Integer> findSubstring(String s, String[] words) {
+    for (String w : words) {
+
+    }
+  }
+
+}
